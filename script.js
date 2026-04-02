afa2415723b9cd0b7e110d088eba1e528b96d76e
opilane <opilane@tpt.lan>
2026-04-02 22:59:18 +1400

    base render code not currently working

---
+++ script.js
@@ -0,0 +1,24 @@
+const world = document.querySelector('#world_contener')
+
+
+
+async function rendermap() {
+  const response = await fetch('https://tinkr.tech/sdb/poly/wander');
+  const data = await response.json();
+  if (!world){return}
+  world.innerHTML = ""
+  for(const player of data.players){
+    const newplayer = document.createElement('div')
+    newplayer.style.position = 'absolute'
+    newplayer.style.left = player.x + 'px'
+    newplayer.style.top = player.y + 'px'
+    const player_sprite = document.createElement('img')
+    player_sprite.src = player.image
+    newplayer.appendChild(player_sprite)
+    world.appendChild(newplayer)
+  }
+}
+
+rendermap()
+
+setInterval(rendermap, 1000);
